da1733777b885c38881c94b99572ef92d11a91e4
Karol Chrobok <karolchrobok@gmail.com>
2023-06-16 11:25:13 +0200

    Add relations to matchModel

--- backend/src/models/matches.ts
+++ backend/src/models/matches.ts
@@ -3,11 +3,10 @@ import {
     Model
 } from 'sequelize';
 import SequelizeConnection from '../services/sequelize-connection';
+import ProfileModel from './profile';
 
 export default class MatchModel extends Model {
     declare id: number;
-    declare customer_profile_id: number;
-    declare professional_profile_id: number;
 }
 
 MatchModel.init({
@@ -19,15 +18,30 @@ MatchModel.init({
     customer_profile_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        unique: true,
+        references: {
+            model: ProfileModel,
+            key: 'id'
+        }
     },
     professional_profile_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        unique: true,
+        references: {
+            model: ProfileModel,
+            key: 'id'
+        }
     },
 }, {
     modelName: 'match',
     timestamps: false,
     sequelize: SequelizeConnection.instance(),
+    indexes: [
+        {
+            unique: true,
+            fields: ['customer_profile_id', 'professional_profile_id']
+        }
+    ]
 });
+
+MatchModel.hasOne(ProfileModel, { foreignKey: 'customer_profile_id' });
+MatchModel.hasOne(ProfileModel, { foreignKey: 'professional_profile_id' });
